c1217431affb06503c9688b95ccfd4a7d62fe4ec
longvibol <vibollong@gmail.com>
2025-12-08 14:49:03 +0700

    sdre

--- app1/bonus/bonus2.3.py
+++ app1/bonus/bonus2.3.py
@@ -1,23 +1,28 @@
 todos = []
 
 while True:
-    user_action = input("Type add, edit or show: ").strip()
+    user_action = input("Type add, edit or show or exit: ").strip()
 
     match user_action:
         case "add":
             todo = input("Enter a todo item: ")
             todos.append(todo)
         case "show" | "s":
-            for item in todos:
-                item = item.title()
-                print(item)
+            for index, item in enumerate(todos):
+                row= f"{index+1}. {item}"
+                print(row)
+            # print(f"==>length {index + 1}")
         case "edit":
-            print(todos)
             number = int(input("Enter a number: ")) -1
             print(todos[number])
             edit_item = input("Please input your edit: ")
             todos[number] = edit_item
             print(todos)
+        case "com":
+            for index, item in enumerate(todos):
+                print(index +1,"-", item)
+            number = int(input("Enter a number: ")) - 1
+            todos.pop(number)
         case "exit":
             break
         case _:
