4293cb22c99f9a36581a7c296fdcc5c99cd5271b
Your Namem <vibollong@gmail.com>
2025-12-31 15:38:46 +0700

    finish

--- Session22_API/API_App/main.py
+++ Session22_API/API_App/main.py
@@ -1,4 +1,7 @@
+from operator import index
+
 import requests
+from send_email import send_email
 
 api_key = "00d73fe42a6145498e2dd76c19732e8f"
 url=("https://newsapi.org/v2/everything?q=tesla&from=2025-11-30&sortBy=publishedAt&apiKey="
@@ -12,6 +15,13 @@ content=request.json()
 
 
 #Access the article title and description
+body = ""
 for article in content["articles"]:
-     print(article["title"])
-     print(article["description"])
+     if article["title"] and article["description"] is not None:
+          body = body + article["title"] +":"+ "\n" + article["description"] + 2*"\n"
+
+body = body.encode('utf-8')
+send_email(body)
+
+body = body.decode('utf-8')
+print(body)
