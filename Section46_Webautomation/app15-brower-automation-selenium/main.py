e5e207d95dc6f649371660813c771c035418c1f5
longvibol <vibollong@gmail.com>
2026-02-04 14:09:43 +0700

    finished

--- Section46_Webautomation/app15-brower-automation-selenium/main.py
+++ Section46_Webautomation/app15-brower-automation-selenium/main.py
@@ -2,48 +2,100 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import ElementClickInterceptedException
+import os
+import time
 
-# ===== CREATE DRIVER =====
-driver = webdriver.Chrome()
-driver.maximize_window()
+class WebAutomation:
+    def __init__(self):
+        download_path = os.path.abspath(os.getcwd())
+        # print("Download folder:", download_path)
 
-# ===== OPEN PAGE =====
-driver.get("https://demoqa.com/login")
+        chrome_options = Options()
+        chrome_options.add_argument("--disable-search-engine-choice-screen")
 
-wait = WebDriverWait(driver, 15)
+        prefs = {
+            "download.default_directory": download_path,
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing.enabled": True
+        }
+        chrome_options.add_experimental_option("prefs", prefs)
 
-try:
-    # ===== LOCATE ELEMENTS =====
-    username_field = wait.until(
-        EC.visibility_of_element_located((By.ID, "userName"))
-    )
+        # ----- FIX HERE -----
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver.maximize_window()
+        self.wait = WebDriverWait(self.driver, 10)
 
-    password_field = wait.until(
-        EC.visibility_of_element_located((By.ID, "password"))
-    )
+    def login(self, username, password):
+        self.driver.get("https://demoqa.com/login")
 
-    login_button = wait.until(
-        EC.element_to_be_clickable((By.ID, "login"))
-    )
+        username_field = self.wait.until(
+            EC.visibility_of_element_located((By.ID, "userName"))
+        )
+        password_field = self.wait.until(
+            EC.visibility_of_element_located((By.ID, "password"))
+        )
+        login_button = self.driver.find_element(By.ID, 'login')
 
-    # ===== FILL FORM =====
-    username_field.send_keys("tech")
-    password_field.send_keys("Money@168")
+        username_field.send_keys(username)
+        password_field.send_keys(password)
 
-    # ===== CLICK LOGIN =====
-    login_button.click()
+        self.driver.execute_script("arguments[0].click();", login_button)
 
-    # ✅ WAIT AFTER LOGIN – VERY IMPORTANT
-    wait.until(
-        EC.visibility_of_element_located((By.ID, "userName-value"))
-    )
+    def fill_form(self, fullname, email, current_address, permanent_address):
+        elements = self.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span')
+            )
+        )
+        elements.click()
 
-    print("✅ Login successful – page stayed open!")
+        text_box_field = self.wait.until(
+            EC.visibility_of_element_located((By.ID, "item-0"))
+        )
+        text_box_field.click()
 
-except Exception as e:
-    print("❌ ERROR:", e)
+        fullname_field = self.wait.until(EC.visibility_of_element_located((By.ID, "userName")))
+        email_field = self.wait.until(EC.visibility_of_element_located((By.ID, "userEmail")))
+        current_address_field = self.wait.until(EC.visibility_of_element_located((By.ID, "currentAddress")))
+        permanent_address_field = self.wait.until(EC.visibility_of_element_located((By.ID, "permanentAddress")))
+        submit_button = self.wait.until(EC.element_to_be_clickable((By.ID, "submit")))
 
-# ===== KEEP BROWSER OPEN =====
-input("Press Enter to close browser...")
-driver.quit()
+        fullname_field.send_keys(fullname)
+        email_field.send_keys(email)
+        current_address_field.send_keys(current_address)
+        permanent_address_field.send_keys(permanent_address)
+
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView({block:'center'});", submit_button
+        )
+
+        ActionChains(self.driver).move_to_element(submit_button).click().perform()
+
+    def download(self):
+        download = self.wait.until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="item-7"]/span'))
+        )
+        download.click()
+
+        download_button = self.wait.until(
+            EC.visibility_of_element_located((By.ID, "downloadButton"))
+        )
+        download_button.click()
+
+    def close(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    webautomation = WebAutomation()
+    webautomation.login(username='tech', password='Money@168')
+    webautomation.fill_form("Kh camtech","hello@gmail.com","Phnom Penh","Turk Thlar")
+    webautomation.download()
+    input("werwer")
+    webautomation.close()
+
+
