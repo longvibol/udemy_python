024794de0bada16a517dcb019e7ff2c1a1292eb9
longvibol <vibollong@gmail.com>
2026-02-06 16:15:05 +0700

    f

--- Section49_Django/app-17-django-form/mysite/settings.py
+++ Section49_Django/app-17-django-form/mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import EMAIL_BACKEND, EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_PASSWORD
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'vibollong@gmail.com'
+EMAIL_HOST_PASSWORD = 'vdydzkmaukvebhik'
+
