83e2a0ec49fc2cac1c367be1d3a1a8d6b130c03b
longvibol <vibollong@gmail.com>
2026-02-09 16:46:47 +0700

    prepare updload

---
+++ Section57_Build_Publish_Python_Package/app20-python-package/main.py
@@ -0,0 +1,5 @@
+from invoicing import generate
+
+generate("invoices", "output", "pythonhow.png",
+                       "product_id","product_name","amount_purchased",
+                       "price_per_unit","total_price")
